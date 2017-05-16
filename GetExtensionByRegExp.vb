d57121836d291adc1df31328bd3ae7f75c46f6c4
Calebe Cavalcante <calebesantos.cavalcante@gmail.com>
2017-05-15 22:10:21 -0300

    Add files via upload

---
+++ GetExtensionByRegExp.vb
@@ -0,0 +1,46 @@
+	'@Author: Calebe Cavalcante <calebesantos.cavalcante@gmail.com>
+    
+    '@Parameters: 
+	'#	sFileName: Esperado nome de arquivo com a extensão no final
+	
+    '@Return: (String) Extensão do Arquivo passado
+	
+    '@Exemplo: GetExtensionByRegExp('Arquivo.extensao') Return: string(extensao)
+    
+    Function GetExtensionByRegExp(sFileName As String) As String
+    Dim regex As Object, str As String
+        
+        On Error GoTo Fail
+        
+        ' Criando novo objeto RegExp
+        Set regex = CreateObject("VBScript.RegExp")
+        
+        ' Padrão Para pegar os últimos caracteres do final do arquivo
+        str = "\.\w{2,}$"
+        
+        'Carregando padrão no objeto regex
+        With regex
+          .Pattern = str
+          .Global = True
+        End With
+        
+        'Verificando se o padrão é encontrado
+        If regex.Test(sFileName) Then
+            ' Retornando a extensão fazendo:
+            ' 1 replace regex.Replace, tirando a extensão, deixando apenas o nome do arquivo
+            ' 2 replace interno, tirando o nome, deixando assim só a extensão
+            ' 3 replace externo, tirando o "." da extensão
+            GetExtensionByRegExp = Replace(Replace(sFileName, regex.Replace(sFileName, ""), ""), ".", "")
+        Else
+            GetExtensionByRegExp = Empty
+        End If
+        
+        ' Finalizando variável
+        Set regex = Nothing
+        
+        Exit Function
+Fail:
+        ' Qualquer erro na execução, retornar vazio
+        GetExtensionByRegExp = Empty
+        
+    End Function
---
+++ PathValidator.vb
@@ -0,0 +1,35 @@
+    '@Author: Calebe Cavalcante <calebesantos.cavalcante@gmail.com>
+    
+    '@Parameters: 
+	'#	sPath: Esperado nome de arquivo com a extensão ou diretório 
+	'#	IsFolderOnly: se true indica que o valor passado é de uma pasta (Folder) e não de um arquivo com extensão
+	
+    '@Return: (Boolean) Retorna True se o Arquivo ou Pasta foi encontrado com sucesso
+	
+    '@Exemplo: PathValidator('c:\users\Desktop\Arquivo.extensao') Return: true
+	
+	Function PathValidator(sPath As String, Optional IsFolderOnly As Boolean) As Boolean
+    Dim sCheck As String
+    
+        On Error GoTo Fail
+        
+        ' Se a variável IsFolderOnly indica que está sendo passado em sPath apenas o diretório sem o nome do arquivo
+        If IsFolderOnly Then
+            sCheck = Dir(sPath, vbDirectory)
+        Else
+            sCheck = Dir(sPath, vbNormal)
+        End If
+        
+        ' Se o valor de retorno da função Dir for empty então retornar como False
+        If sCheck = "" Or IsEmpty(sCheck) Then
+            PathValidator = False
+        Else
+            PathValidator = True
+        End If
+        
+        Exit Function
+        
+Fail:
+        ' Qualquer erro na execução do código retorna como false
+        PathValidator = False
+    End Function
