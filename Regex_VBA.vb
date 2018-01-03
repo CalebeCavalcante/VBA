2d9f798d530a767850b0dcb1f3d081e5086ffe76
Calebe Cavalcante <calebesantos.cavalcante@gmail.com>
2018-01-03 16:35:16 -0200

    Update Regex_VBA.vb

--- Regex_VBA.vb
+++ Regex_VBA.vb
@@ -1,11 +1,25 @@
 
 ' Fonte: http://analystcave.com/excel-regex-tutorial/
 
-' Like Operator
+' ############## Like Operator ##############
+
+' Fonte: http://analystcave.com/vba-like-operator/
+' Regras para o Like Operator 
+' * matches any number of characters
+' ? matches any 1 character
+' [ ] matches any 1 character specified between the brackets
+' - matches any range of characters e.g. [a-z] matches any non-capital 1 letter of the alphabet
+' # matches any digit character
 
 If "Animal" Like "[A-Z]*" then 
    Debug.Print "Match: String starting with Capital letter!"
 End If
+
+If "3Q/2017" Like "#[Q|T|S]/#*" Then
+   Debug.Print "Data Agrupada por Q, S ou T"
+End If
+
+' ############## Regex ##############
 
 ' TESTE MATCHING
 
